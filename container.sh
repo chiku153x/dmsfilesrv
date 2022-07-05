fcc59d09d4ff06dd529f2d578323a7d8eb393b34
Gayan Chinthaka <chinthaka@blackswan-technologies.com>
2022-07-05 15:23:28 +0530

    changed the name of the conmtainer

--- container.sh
+++ container.sh
@@ -4,7 +4,7 @@ if [[ -z "${D_TAG_VERSION}" ]]; then
 else
 	TAG=dmsfile:${D_TAG_VERSION}
 fi
-NAME=dmsrest
+NAME=dmsfile
 docker rm -f ${NAME} || true
 docker run -d -it --rm --name ${NAME} \
 	-p 3000:3000 \
